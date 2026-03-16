66b935bfc166fd0b61a4342cb64cb5ee551e28ae
d3uceY <onyekwelujesse1234@gmail.com>
2026-03-16 12:32:54 +0100

    feat: add GitHub Actions workflow for deploying documentation to GitHub Pages and update Docusaurus config for base URL

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,12 +18,13 @@ const config: Config = {
   url: 'https://d3uceY.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Cokpyt/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'd3uceY', // Usually your GitHub org/user name.
   projectName: 'Cokpyt', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
