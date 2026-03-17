d391412b01f3a459280118f266c97ac48675d0e3
d3uceY <onyekwelujesse1234@gmail.com>
2026-03-17 10:00:22 +0100

    fix: disable blog feature in docusaurus configuration

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -47,21 +47,7 @@ const config: Config = {
           editUrl:
             'https://github.com/d3uceY/Cokpyt/tree/main/docs/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/d3uceY/Cokpyt/tree/main/docs/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -100,7 +86,6 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/d3uceY/Cokpyt',
           label: 'GitHub',
